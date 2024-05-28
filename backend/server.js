a997430c2acf3aaa8863b317ecd5b401ac4a174a
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-05-28 16:51:02 +0300

    cleaning up

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,40 @@
+// Global Imports
 import express from 'express';
 import dotenv from 'dotenv';
-import user from './routes/user.js';
-
-dotenv.config();
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import { connect_db } from './config/db.js';
 import cookieParser from 'cookie-parser';
 
+// Configuration for dotenv
+dotenv.config();
+
+// Local Imports
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import { connect_db } from './config/db.js';
+import user from './routes/user.js';
+
+// Constants
 const port = process.env.PORT || 5000;
 
+// Express App
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Routes for Users
 app.use("/api/users", user);
 
+// Default Route
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
+// Error Handling Middleware
 app.use(notFound);
 app.use(errorHandler);
 
+// Start Server
 const start_server = async () => {
     try {
         connect_db();
