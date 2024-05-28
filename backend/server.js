02dcd87e0b13643ee0021277881a8998cb4f9a1f
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-05-28 17:10:36 +0300

    Changes to be committed: 	modified:   backend/server.js 	modified:   package-lock.json 	modified:   package.json

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import morgan from 'morgan';
 
 // Configuration for dotenv
 dotenv.config();
@@ -21,6 +23,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
+app.use(morgan('dev'));
 
 // Routes for Users
 app.use("/api/users", user);
